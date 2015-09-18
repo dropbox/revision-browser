1e4183e27bec21b6c2149ea71949bafae73b3888
Eric Feng <erichfeng@gmail.com>
2015-09-18 16:41:06 -0400

    ripping out flask login and sqlalchemy

--- app.py
+++ app.py
@@ -14,44 +14,13 @@ from flask import (
 	session,
 	url_for
 	)
-from flask.ext.login import (
-	current_user,
-	LoginManager,
-	login_required,
-	login_user,
-	logout_user,
-	UserMixin
-	)
-from flask.ext.sqlalchemy import SQLAlchemy
 
 APP_KEY = os.environ['APP_KEY']
 APP_SECRET = os.environ['APP_SECRET']
 
 app = Flask(__name__)
-app.config['DEBUG'] = os.environ['DEBUG'] == 'True'
+app.config['DEBUG'] = os.environ.get('DEBUG') == 'True'
 app.secret_key = os.environ['FLASK_SECRET_KEY']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-db = SQLAlchemy(app)
-
-class User(db.Model, UserMixin):
-	id = db.Column(db.Text, primary_key=True)
-	name = db.Column(db.Text)
-	email = db.Column(db.Text)
-	access_token = db.Column(db.Text)
-
-	def __init__(self, id, name, email, access_token):
-		self.id = id
-		self.name = name
-		self.email = email
-		self.access_token = access_token
-
-@login_manager.user_loader
-def load_user(id):
-	return User.query.filter_by(id = id).first()
 
 def get_url(route):
     '''Generate a proper URL, forcing HTTPS if not running locally'''
@@ -60,7 +29,6 @@ def get_url(route):
                   _external=True,
                   _scheme='http' if host in ('127.0.0.1', 'localhost') else
                   'https')
-
     return url
 
 def get_dropbox_auth_flow():
@@ -75,22 +43,15 @@ def login():
 def oauth_callback():
     '''Callback function for when the user returns from OAuth.'''
     access_token, user_id, url_state = get_dropbox_auth_flow().finish(request.args)
-    dbx = Dropbox(access_token)
-    account = dbx.users_get_current_account()
-    user = User.query.filter_by(id = account.account_id).first()
-
-    if not user:
-    	user = User(account.account_id, account.name.display_name, account.email, access_token)
-    	db.session.add(user)
-    	db.session.commit()
-
-    login_user(user)
-
+    session['access_token'] = access_token
     return redirect(url_for('index'))
 
-@app.route("/logout")
+@app.route('/logout')
 def logout():
-	logout_user()
+	if session.get('access_token'):
+		requests.post('https://api.dropboxapi.com/1/disable_access_token',
+			headers={'Authorization': 'Bearer ' + session['access_token']})
+	session.clear()
 	return redirect(url_for('index'))
 
 @app.route("/")
@@ -98,23 +59,24 @@ def index():
 	return render_template('index.html')
 
 @app.route('/revisions')
-@login_required
 def revisions():
 	link = request.args['link']
 	metadata = requests.post('https://api.dropbox.com/1/metadata/link', params={'link': link},
-		headers={'Authorization': 'Bearer ' + str(current_user.access_token)}).json()
-	dbx = Dropbox(current_user.access_token)
+		headers={'Authorization': 'Bearer ' + str(session['access_token'])}).json()
+	dbx = Dropbox(session['access_token'])
 	revisions_result = dbx.files_list_revisions(metadata['path'])
+
 	if not revisions_result.is_deleted:
-		return render_template('revisions.html', path=metadata['path'], revisions=revisions_result.entries)
+		return render_template('revisions.html', path=metadata['path'],
+			revisions=revisions_result.entries)
 	else:
 		redirect(url_for('index'))
 
 @app.route('/revision')
-@login_required
 def revision():
-	dbx = Dropbox(current_user.access_token)
+	dbx = Dropbox(session['access_token'])
 	f = dbx.files_download(request.args['path'], request.args['rev'])
+
 	resp = make_response(f[1].content)
 	resp.headers["Content-Disposition"] = "attachment; filename=" + f[0].name
 	return resp
